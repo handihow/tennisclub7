be62dd235c03ce5fffe61bb4eddca9c94e665d14
Roeland Verbakel <roeland@handihow.com>
2020-03-19 23:30:44 +0100

    add excel spreadsheet to email attachments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,53 @@
 import * as functions from 'firebase-functions';
-// import * as admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
 const sgMail = require('@sendgrid/mail');
 const dateFormat = require('dateformat');
+const db = admin.firestore();
+const Excel = require('exceljs');
 
-
-export const sendEmails = functions.firestore.document('registrations/{id}').onWrite((change, context) => {
+export const sendEmails = functions.firestore.document('registrations/{id}').onWrite(async (change, context) => {
 	const newValue = change.after.data();
 	const previousValue = change.before.data();
 	if(newValue && previousValue && newValue.isFinished && !previousValue.isFinished){
 		//set the api key for sendgrid
 		sgMail.setApiKey(functions.config().sendgrid.key);
 		const data = newValue.data;
-		let attachments = [];
+		const attachmentsToMember = [];
+		const attachmentsToAdministration = []
 		try{
 			const image = data.image;
 			const signature : string = data.signature;
 			const signatureBase64 = signature.split(',')[1];
 			const signatureFileType = signature.split(',')[0].split(';')[0].split(':')[1];
 			const signatureFileName = data.firstName + '_' + data.lastName + '_' + 'handtekening.png'
-			attachments.push({
+			const imageAttachment = {
 		      content: image[0].content.split(',')[1],
 		      filename: image[0].name,
 		      type: image[0].type,
 		      disposition: "attachment"
-		    });
-		    attachments.push({
+		    };
+			attachmentsToMember.push(imageAttachment);
+			attachmentsToAdministration.push(imageAttachment);
+			const signatureAttachment = {
 		    	content: signatureBase64,
 		    	filename: signatureFileName,
 		    	type: signatureFileType,
 		    	disposition: "attachment"
-		    });
+		    };
+		    attachmentsToMember.push(signatureAttachment);
+		    attachmentsToAdministration.push(signatureAttachment);
+			const excelBuffer = await createExcel();
+			if(Buffer.isBuffer(excelBuffer)){
+				const base64File = excelBuffer.toString('base64');
+				attachmentsToAdministration.push({
+					content: base64File,
+					filename: 'Overzicht alle inschrijvingen.xlsx',
+					type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+					disposition: "attachment"
+				})
+			}
 		} catch(e){
 			console.error(e.toString());
 		}
@@ -58,15 +76,15 @@ export const sendEmails = functions.firestore.document('registrations/{id}').onW
 			fromname: 'Ledenadministratie Tennisclub7',
 			templateId: 'd-06de23332fd04c43a1258a1c99f090bb',
 			dynamic_template_data: dynamicData,
-			attachments: attachments
+			attachments: attachmentsToMember
 		};
 		const msgToAdministration = {
-			to: 'ewoudverbakel@gmail.com',
+			to: 'ledenadministratie@tennisclub7.nl',
 			from: 'no-reply@tennisclub7.nl',
 			fromname: 'Ledenadministratie Tennisclub7',
 			templateId: 'd-08a21deec3c9430ab552406395ed0c61',
 			dynamic_template_data: dynamicData,
-			attachments: attachments
+			attachments: attachmentsToAdministration
 		}
 	    return sgMail.send(msgToAdministration)
 		    .then(() => {
@@ -88,6 +106,57 @@ export const sendEmails = functions.firestore.document('registrations/{id}').onW
 		console.log('not sending email, the user is not yet finished');
 		return false;
 	}
-	
 
 })
+
+const createExcel = async () => {
+	const registrationSnap = await db.collection('registrations').orderBy('updatedAt', 'asc').get();
+	if(registrationSnap.empty){
+		return null;
+	} else {
+		const workbook = new Excel.Workbook();
+		workbook.creator = 'HandiHow';
+		workbook.lastModifiedBy = 'NodeJS automatic function by HandiHow';
+		workbook.created = new Date();
+		const sheet = workbook.addWorksheet('Inschrijvingen');
+		sheet.columns = [
+		  { header: 'Email', key: 'email', width: 30 },
+		  { header: 'Voornaam', key: 'firstName', width: 20 },
+		  { header: 'Achternaam', key: 'lastName', width: 20 },
+		  { header: 'Geslacht', key: 'gender', width: 10 },
+		  { header: 'Geb. datum', key: 'birthDate', width: 10 },
+		  { header: 'Adres', key: 'address', width: 20},
+		  { header: 'Postcode', key: 'postalCode', width: 10},
+		  { header: 'Plaatsnaam', key: 'city', width: 20},
+		  { header: 'Mobiel', key: 'mobilePhoneNumber', width: 20},
+		  { header: 'Lidmaatschap', key: 'membershipType', width: 30},
+		  { header: 'Lid per', key: 'date', width: 10},
+		  { header: 'Enkelspel', key: 'currentRatingSingles', width: 10},
+		  { header: 'Dubbelspel', key: 'currentRatingDoubles', width: 10}
+		];
+		const registrationRecords = registrationSnap.docs.map(d => d.data());
+		registrationRecords.forEach(record => {
+			const data = record.data;
+			sheet.addRow({
+			  email: data.email,
+	          firstName: data.firstName,
+	          lastName: data.lastName,
+	          gender: data.gender,
+	          birthDate: dateFormat(data.birthDate, "d-m-yyyy"),
+	          address: data.address,
+	          postalCode: data.postalCode,
+	          city: data.city,
+	          mobilePhoneNumber: data.mobilePhoneNumber,
+	          membershipType: data.membershipType,
+	          date: dateFormat(data.date, "d-m-yyyy"),
+	          currentRatingSingles: data.currentRatingSingles ? data.currentRatingSingles : '-',
+	          currentRatingDoubles: data.currentRatingDoubles ? data.currentRatingDoubles : '-'
+			});
+		});
+		return workbook.xlsx.writeBuffer()
+		.then((buffer: Buffer) => {
+			return buffer;
+		});
+	}
+}
+
